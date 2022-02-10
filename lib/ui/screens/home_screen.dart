c50268bca4fa9b52e36b5bff0dfd1c68f9c1ca72
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2022-02-11 00:29:12 +0530

    Update home_screen.dart
    - Added lazy loading in home screen
    - Added loading effect while loading first page

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:wallpaper_app/ui/widgets/loading_widget.dart';
 import 'package:wallpaper_app/ui/widgets/search_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late PagingController<int, String> _pagingController;
+  @override
+  void initState() {
+    _pagingController = PagingController(firstPageKey: 0);
+    _fetchPage(0);
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      await Future.delayed(const Duration(seconds: 3));
+      final newItems = ["Anil", "Akhil", "Arun", "Stefy"];
+      final isLastPage = pageKey == 15;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,41 +63,41 @@ class HomeScreen extends StatelessWidget {
               ),
               flexibleSpace: SearchBar(),
             ),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                color: Colors.red,
-                height: 200,
+            PagedSliverGrid<int, String>(
+              showNewPageProgressIndicatorAsGridChild: false,
+              showNewPageErrorIndicatorAsGridChild: false,
+              showNoMoreItemsIndicatorAsGridChild: false,
+              pagingController: _pagingController,
+              builderDelegate: PagedChildBuilderDelegate<String>(
+                itemBuilder: (context, item, index) {
+                  return Container(
+                    height: 200,
+                    width: 300,
+                    color: Colors.red,
+                    child: Text(item),
+                  );
+                },
+                firstPageProgressIndicatorBuilder: (context) => SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: GridView.builder(
+                    itemCount: 10,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3 / 4,
+                    ),
+                    itemBuilder: (_, __) => const LoadingWidget(),
+                  ),
+                ),
               ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                color: Colors.red,
-                height: 200,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 5.0,
+                crossAxisSpacing: 5.0,
+                childAspectRatio: 3 / 4,
               ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                color: Colors.red,
-                height: 200,
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                color: Colors.red,
-                height: 200,
-              ),
-            ),
-            SliverToBoxAdapter(
-              child: Container(
-                margin: const EdgeInsets.symmetric(vertical: 20),
-                color: Colors.red,
-                height: 200,
-              ),
-            ),
+            )
           ],
         ),
       ),
