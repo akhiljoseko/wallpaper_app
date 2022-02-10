69c9ecbba3ff30b1579392715cbd1ef31189da6f
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2022-02-10 21:32:50 +0530

    Update home_screen.dart
    - Created a dummy ui for home screen with search bar and containers in body

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaper_app/ui/widgets/search_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              floating: true,
+              snap: true,
+              stretch: true,
+              expandedHeight: 150,
+              elevation: 5.0,
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              flexibleSpace: SearchBar(),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                color: Colors.red,
+                height: 200,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                color: Colors.red,
+                height: 200,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                color: Colors.red,
+                height: 200,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                color: Colors.red,
+                height: 200,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                color: Colors.red,
+                height: 200,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
