75f87577a1e9b62641e92a9860fd1a95e81a4bbe
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2022-02-11 21:28:01 +0530

    Update home_screen.dart
    - Removed scroll controller instead of that used notification listener
    - Changed gridview cross axis count

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:wallpaper_app/models/photo_data.dart';
 import 'package:wallpaper_app/providers/search_result_provider.dart';
+import 'package:wallpaper_app/ui/screens/image_screen.dart';
+import 'package:wallpaper_app/ui/widgets/image_preview_container.dart';
 import 'package:wallpaper_app/ui/widgets/loading_widget.dart';
 import 'package:wallpaper_app/ui/widgets/search_bar.dart';
 
@@ -14,15 +16,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late PagingController<int, PhotoData> _pagingController;
-  late ScrollController _scrollController;
   String searchQuery = "";
   @override
   void initState() {
-    _scrollController = ScrollController();
-    _scrollController
-        .addListener(() => FocusManager.instance.primaryFocus?.unfocus());
-    _pagingController = PagingController(firstPageKey: 0);
-    _fetchPage(0);
+    _pagingController = PagingController(firstPageKey: 1);
     _pagingController.addPageRequestListener((pageKey) => _fetchPage(pageKey));
     super.initState();
   }
@@ -35,14 +32,13 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<void> _fetchPage(int pageKey) async {
     try {
-      await Future.delayed(const Duration(seconds: 3));
       final newItems =
-          await SearchResultProvider().getSearchResult(searchQuery);
-      final isLastPage = pageKey == 15;
+          await SearchResultProvider().getSearchResult(searchQuery, pageKey);
+      final isLastPage = newItems.length < 20;
       if (isLastPage) {
         _pagingController.appendLastPage(newItems);
       } else {
-        final nextPageKey = pageKey + newItems.length;
+        final nextPageKey = pageKey + 1;
         _pagingController.appendPage(newItems, nextPageKey);
       }
     } catch (error) {
@@ -53,66 +49,67 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black,
       body: SafeArea(
-        child: CustomScrollView(
-          controller: _scrollController,
-          slivers: [
-            SliverAppBar(
-              floating: true,
-              snap: true,
-              stretch: true,
-              expandedHeight: 150,
-              elevation: 5.0,
-              shape: const ContinuousRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+        child: NotificationListener<ScrollUpdateNotification>(
+          onNotification: (ScrollUpdateNotification notification) {
+            final FocusScopeNode focusScope = FocusScope.of(context);
+            if (notification.dragDetails != null &&
+                focusScope.hasFocus &&
+                notification.scrollDelta! > 10.0) {
+              focusScope.unfocus();
+            }
+            return false;
+          },
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: Container(
+                  width: double.maxFinite,
+                  height: 150,
+                  child:
+                      SearchBar(onSearchPressed: (s) => _updateSearchQuery(s)),
                 ),
               ),
-              flexibleSpace: SearchBar(
-                onSearchPressed: (query) => _updateSearchQuery(query),
-              ),
-            ),
-            PagedSliverGrid<int, PhotoData>(
-              showNewPageProgressIndicatorAsGridChild: false,
-              showNewPageErrorIndicatorAsGridChild: false,
-              showNoMoreItemsIndicatorAsGridChild: false,
-              pagingController: _pagingController,
-              builderDelegate: PagedChildBuilderDelegate<PhotoData>(
-                itemBuilder: (context, item, index) {
-                  return Container(
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                      image: DecorationImage(
-                        image: NetworkImage(item.webformatUrl),
-                        fit: BoxFit.cover,
+              PagedSliverGrid<int, PhotoData>(
+                showNewPageProgressIndicatorAsGridChild: false,
+                showNewPageErrorIndicatorAsGridChild: false,
+                showNoMoreItemsIndicatorAsGridChild: false,
+                pagingController: _pagingController,
+                builderDelegate: PagedChildBuilderDelegate<PhotoData>(
+                  itemBuilder: (context, item, index) {
+                    return ImagePreviewContainer(
+                      photoData: item,
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ImagePreviewScreen(photoData: item),
+                        ),
                       ),
+                    );
+                  },
+                  firstPageProgressIndicatorBuilder: (context) => SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    child: GridView.builder(
+                      itemCount: 12,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        childAspectRatio: 3 / 4,
+                      ),
+                      itemBuilder: (_, __) => const LoadingWidget(),
                     ),
-                  );
-                },
-                firstPageProgressIndicatorBuilder: (context) => SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  child: GridView.builder(
-                    itemCount: 10,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 3 / 4,
-                    ),
-                    itemBuilder: (_, __) => const LoadingWidget(),
                   ),
                 ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 3 / 4,
+                ),
               ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 5.0,
-                crossAxisSpacing: 5.0,
-                childAspectRatio: 3 / 4,
-              ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
