440154cba0a075141acde4fea8e9c7ff3ddee490
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2022-02-11 18:51:12 +0530

    Update search_bar.dart
    - Added call back for initialising  search once search submitted

--- lib/ui/widgets/search_bar.dart
+++ lib/ui/widgets/search_bar.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class SearchBar extends StatelessWidget {
-  const SearchBar({Key? key}) : super(key: key);
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key? key, required this.onSearchPressed}) : super(key: key);
+
+  final void Function(String) onSearchPressed;
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +30,19 @@ class SearchBar extends StatelessWidget {
           children: [
             Expanded(
               child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-                  hintText: "Search",
-                  hintStyle: Theme.of(context)
-                      .textTheme
-                      .bodyText1
-                      ?.copyWith(color: Colors.grey),
-                  border: InputBorder.none,
-                ),
-              ),
+                  controller: _controller,
+                  textInputAction: TextInputAction.search,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                    hintText: "Search",
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        ?.copyWith(color: Colors.grey),
+                    border: InputBorder.none,
+                  ),
+                  onSubmitted: (value) =>
+                      widget.onSearchPressed(_controller.text)),
             ),
             Container(
               margin: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
@@ -33,7 +51,7 @@ class SearchBar extends StatelessWidget {
                 color: Theme.of(context).primaryColor,
               ),
               child: IconButton(
-                onPressed: () {},
+                onPressed: () => widget.onSearchPressed(_controller.text),
                 icon: const Icon(
                   Icons.search,
                   color: Colors.white,
