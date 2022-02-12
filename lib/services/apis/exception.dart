a6c34192001d9c221144f2c9dd61161074201652
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2022-02-12 13:29:57 +0530

    Source code documented

--- lib/services/apis/exception.dart
+++ lib/services/apis/exception.dart
@@ -1,19 +1,24 @@
+/// Custom [Exception] made to throw when the API returned a error of BAD_REQUEST
 class BadRequestException implements Exception {
   BadRequestException([message]);
 }
 
+/// Custom [Exception] made to throw when the API returned a error of AUTHORIZATION
 class UnauthorizedException implements Exception {
   UnauthorizedException([message]);
 }
 
+/// Custom [Exception] made to throw when the API returned a error of NOT_FOUND
 class NotFoundException implements Exception {
   NotFoundException([message]);
 }
 
+/// Custom [Exception] made to throw when the API returned a error of INTERNAL_SERVER
 class InternalServerErrorException implements Exception {
   InternalServerErrorException([message]);
 }
 
+/// Custom [Exception] made to throw when the system detect a error of NO_INTERNET
 class NoInternetException implements Exception {
   NoInternetException([message]);
 }
--- lib/services/apis/search_api.dart
+++ lib/services/apis/search_api.dart
@@ -1,5 +1,6 @@
 import 'package:wallpaper_app/services/apis/base_api.dart';
 
+/// API class for handling calls to search api
 class SearchApi extends BaseApi {
   Future<dynamic> getSearchResult(String params) async {
     return await super.get(params);
